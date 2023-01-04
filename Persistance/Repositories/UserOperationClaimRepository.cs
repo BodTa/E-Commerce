2180e2cc7a7d84de915e680a3046da608506cea4
Taner Özdemir <tanerozd16@gmail.com>
2023-01-04 23:16:35 +0300

    User Operation Claim's relation added.

--- Persistance/Repositories/UserOperationClaimRepository.cs
+++ Persistance/Repositories/UserOperationClaimRepository.cs
@@ -1,9 +1,16 @@
 ﻿
 
 using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistance.Contexts;
 
 namespace Persistance.Repositories;
 
-public class UserOperationClaimRepository : IUserOperationClaimRepository
+public class UserOperationClaimRepository :EfRepositoryBase<UserOperationClaim,BaseDbContext> ,IUserOperationClaimRepository
 {
+	public UserOperationClaimRepository(BaseDbContext context) : base(context)
+	{
+
+	}
 }
