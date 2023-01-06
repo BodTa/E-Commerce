a5dff4ed5063659bd846e7192151b8035229683b
Taner Özdemir <tanerozd16@gmail.com>
2023-01-06 13:01:43 +0300

    Revoke Token by Request Coded

--- Application/Features/Auths/Commands/RevokeToken/RevokeTokenCommand.cs
+++ Application/Features/Auths/Commands/RevokeToken/RevokeTokenCommand.cs
@@ -1,7 +1,40 @@
 ﻿
 
+using Application.Features.Auths.DTOs;
+using Application.Features.Auths.Rules;
+using Application.Services.AuthService;
+using AutoMapper;
+using MediatR;
+
 namespace Application.Features.Auths.Commands.RevokeToken;
 
-public class RevokeTokenCommand
+public class RevokeTokenCommand : IRequest<RevokedTokenDto>
 {
+    public string Token { get; set; }
+    public string IpAdress { get; set; }
+}
+
+public class RevokeTokenCommandHandler : IRequestHandler<RevokeTokenCommand, RevokedTokenDto>
+{
+    private readonly IAuthService _authService;
+    private readonly AuthBusinessRules _authBusinessRules;
+    private readonly IMapper _mapper;
+
+    public RevokeTokenCommandHandler(IAuthService authService, AuthBusinessRules authBusinessRules, IMapper mapper)
+    {
+        _authService = authService;
+        _authBusinessRules = authBusinessRules;
+        _mapper = mapper;
+    }
+
+    public async Task<RevokedTokenDto> Handle(RevokeTokenCommand request, CancellationToken cancellationToken)
+    {
+        var refreshToken = await _authService.GetRefreshTokenByToken(request.Token);
+        await _authBusinessRules.RefreshTokenShouldBeExist(refreshToken);
+        await _authBusinessRules.RefreshTokenShouldBeActive(refreshToken);
+
+        await _authService.RevokeRefreshToken(refreshToken, request.IpAdress, "Revoked without replacement");
+
+        return _mapper.Map<RevokedTokenDto>(refreshToken);
+    }
 }
