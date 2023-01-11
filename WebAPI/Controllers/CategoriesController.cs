117dbecf303640f15e253f0972c7534a2cbdd3a1
Taner Özdemir <tanerozd16@gmail.com>
2023-01-12 00:05:50 +0300

    CategoriesController Created&Coded
    Controller methods coded for needed requests.

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+﻿
+using Application.Features.Categories.Commands.CreateCategory;
+using Application.Features.Categories.Commands.DeleteCategory;
+using Application.Features.Categories.Commands.UpdateCategory;
+using Application.Features.Categories.DTOs;
+using Application.Features.Categories.Models;
+using Application.Features.Categories.Queries.GetByIdCategory;
+using Application.Features.Categories.Queries.GetListCategor;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : BaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListCategoryQuery getListCategoryQuery = new() { PageRequest = pageRequest };
+        CategoryListModel result = await Mediator.Send(getListCategoryQuery);
+        return Ok(result);
+    }
+
+    [HttpGet("Id")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdCategoryQuery getByIdCategoryQuery)
+    {
+        CategoryDto result = await Mediator.Send(getByIdCategoryQuery);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateCategoryCommand createCategoryCommand)
+    {
+        CreatedCategoryDto result = await Mediator.Send(createCategoryCommand);
+        return Created("", result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateCategoryCommand updateCategoryCommand)
+    {
+        UpdatedCategoryDto result = await Mediator.Send(updateCategoryCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteCategoryCommand deleteCategoryCommand)
+    {
+        DeletedCategoryDto result = await Mediator.Send(deleteCategoryCommand);
+        return Ok(result);
+    }
+}
