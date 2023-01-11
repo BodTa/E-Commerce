8a0a1b250d1cd1505099230ce14c68e91c66dfb6
Taner Özdemir <tanerozd16@gmail.com>
2023-01-12 00:36:46 +0300

    UOC Controller created&coded
    Controller methods coded for needed requests

---
+++ WebAPI/Controllers/UserOperationClaimsController.cs
@@ -0,0 +1,58 @@
+﻿using Application.Features.Colors.Commands.CreateColor;
+using Application.Features.Colors.Commands.DeleteColor;
+using Application.Features.Colors.Commands.UpdateColor;
+using Application.Features.Colors.DTOs;
+using Application.Features.Colors.Models;
+using Application.Features.Colors.Queries.GetByIdColor;
+using Application.Features.Colors.Queries.GetListColor;
+using Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
+using Application.Features.UserOperationClaims.Commands.DeleteUserOperationClaim;
+using Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim;
+using Application.Features.UserOperationClaims.DTOs;
+using Application.Features.UserOperationClaims.Queries.GetByIdUserOperationClaim;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserOperationClaimsController : BaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListColorQuery getListColorQuery = new() { PageRequest = pageRequest };
+        ColorListModel result = await Mediator.Send(getListColorQuery);
+        return Ok(result);
+    }
+
+    [HttpGet("Id")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdUserOperationClaimQuery getByIdUserOperationClaimQuery)
+    {
+        UserOperationClaimDto result = await Mediator.Send(getByIdUserOperationClaimQuery);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateUserOperationClaimCommand createUserOperationClaimCommand)
+    {
+        CreatedUserOperationClaimDto result = await Mediator.Send(createUserOperationClaimCommand);
+        return Created("", result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateUserOperationClaimCommand updateUserOperationClaimCommand)
+    {
+        UpdatedUserOperationClaimDto result = await Mediator.Send(updateUserOperationClaimCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteUserOperationClaimCommand deleteUserOperationClaimCommand)
+    {
+        DeletedUserOperationClaimDto result = await Mediator.Send(deleteUserOperationClaimCommand);
+        return Ok(result);
+    }
+}
